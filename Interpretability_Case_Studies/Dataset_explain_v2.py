9fa307e24fcf6dc049baa20f48238f122fdd4409
badaozhiwei-cmyk <badaozhiwei@gmail.com>
2026-06-16 22:30:04 +0800

    Fix final indentation error in Dataset_explain_v2.py

--- Interpretability_Case_Studies/Dataset_explain_v2.py
+++ Interpretability_Case_Studies/Dataset_explain_v2.py
@@ -180,7 +180,7 @@ class IL_set_v2(torch.utils.data.Dataset):
         edge_index = torch.tensor(mol[1], dtype=torch.long)
 
         # 单原子离子（?[I-]、[Cl-]）没有化学键，需要特殊处?
-    if len(mol[2]) == 0:
+        if len(mol[2]) == 0:
             edge_attr = torch.zeros((0, 3), dtype=torch.long)
         else:
             edge_attr = torch.tensor(mol[2], dtype=torch.long)
